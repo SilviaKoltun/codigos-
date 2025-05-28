e631815f64705636da0b29c13dfd4f47f4c95182
SilviaKoltun <silviakoltun@gmail.com>
2025-05-28 15:58:58 -0300

    Add files via upload

--- CalculadoraIMC.c
+++ CalculadoraIMC.c
@@ -19,11 +19,13 @@ int main(){
     else if (IMC >=18.5 &&IMC <=24.9){
         printf ("Peso Normal");
     }
-     else if (IMC >=18.5 &&IMC <=24.9){
-        printf ("Peso Normal");
-
+     else if (IMC >=25 &&IMC <=29.9){
+        printf ("Sobre Peso");
+     }
+    else if (IMC >30){
+        printf ("obesidade");
     }
 
-    return 0;
+   return 0;
 
 }
